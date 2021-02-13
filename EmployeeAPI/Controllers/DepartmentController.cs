07282a7e3c66c15ff78fe2b16115fecd72f59b67
masum001 <masumkhalil7@gmail.com>
2021-02-13 22:02:44 +0600

    models and controllers

---
+++ EmployeeAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net.Http;
+using System.Web.Configuration;
+
+
+namespace EmployeeAPI.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            DataTable aDataTable = new DataTable();
+
+            string query = @"SELECT * FROM Department";
+
+            SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["EmployeeDB"].ConnectionString);
+
+            SqlCommand command =new SqlCommand(query, connection);
+
+            connection.Open();
+            aDataTable.Load(command.ExecuteReader());
+            connection.Close();
+
+            return Request.CreateResponse(HttpStatusCode.OK, aDataTable);
+        }
+    }
+}
---
+++ EmployeeAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Configuration;
+using System.Web.Http;
+
+namespace EmployeeAPI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            DataTable aDataTable = new DataTable();
+
+            string query = @"SELECT * FROM Employees";
+
+            SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["EmployeeDB"].ConnectionString);
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            connection.Open();
+            aDataTable.Load(command.ExecuteReader());
+            connection.Close();
+
+            return Request.CreateResponse(HttpStatusCode.OK, aDataTable);
+        }
+    }
+}
