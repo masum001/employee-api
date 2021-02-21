ecca48dd3c23424b40f45ebe97af102c244ee2d7
masum001 <masumkhalil7@gmail.com>
2021-02-21 14:25:34 +0600

    react app setup

---
+++ employee-info-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import {Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Button variant='danger'>Hello</Button>
+    </div>
+  );
+}
+
+export default App;
