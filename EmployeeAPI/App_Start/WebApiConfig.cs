01cb9946ffab127fa34942785564d42cb27f6818
masum001 <masumkhalil7@gmail.com>
2021-02-26 20:21:31 +0600

    CORAS issue fixed

--- EmployeeAPI/App_Start/WebApiConfig.cs
+++ EmployeeAPI/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web.Http;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Web.Http.Cors;
 
 namespace EmployeeAPI
 {
@@ -12,7 +13,7 @@ namespace EmployeeAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            config.EnableCors();
             // Web API routes
             config.MapHttpAttributeRoutes();
 
@@ -23,6 +24,8 @@ namespace EmployeeAPI
             );
 
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add( new MediaTypeHeaderValue("text/html"));
+
+            config.EnableCors(new EnableCorsAttribute("http://localhost:3000","*","*"));
         }
     }
 }
